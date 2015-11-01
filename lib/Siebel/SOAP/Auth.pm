3f07189eaeced950002dad8e558bd65ccd316100
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2015-11-01 01:52:59 -0200

    Including Pod

--- lib/Siebel/SOAP/Auth.pm
+++ lib/Siebel/SOAP/Auth.pm
@@ -17,9 +17,95 @@ use Log::Report 1.05 'siebel-soap-auth', syntax => 'SHORT';
 
 Siebel::SOAP::Auth - Moo based class to implement transparent Siebel Session Management for XML::Compile::WSDL11
 
+=head1 SYNOPSIS
+
+    use XML::Compile::WSDL11;
+	use XML::Compile::SOAP11;
+    use XML::Compile::Transport::SOAPHTTP;
+	use Siebel::SOAP::Auth;
+
+	my $wsdlfile = File::Spec->catfile( 't', 'SWIContactServices.WSDL' );
+    my %request = (
+        ListOfSwicontactio => {
+            Contact =>
+                { Id => '0-1', FirstName => 'Siebel', LastName => 'Administrator' }
+        }
+    );
+
+    my $wsdl = XML::Compile::WSDL11->new($wsdlfile);
+    my $auth = Siebel::SOAP::Auth->new(
+        {
+            user          => 'sadmin',
+            password      => 'XXXXXXX',
+            token_timeout => MAGIC_NUMBER,
+            remain_ttl    => 0
+        }
+    );
+
+    my $call = $wsdl->compileClient(
+        operation      => 'SWIContactServicesQueryByExample',
+        transport_hook => sub { 
+            my ( $request, $trace, $transporter ) = @_;
+            # request was modified
+            my $new_request = $auth->add_auth_header($request);
+            return $trace->{user_agent}->request($new_request);
+        }
+    );
+
+    my ( $answer, $trace ) = $call->(%request);
+
+    if ( my $e = $@->wasFatal ) {
+
+        $e->throw;
+
+    } else {
+
+        # do something with the answer
+
+	}
+
+=head1 DESCRIPTION
+
+Siebel::SOAP::Auth implements authentication for Oracle's Siebel inbound webservices by implementing Session Management.
+
+Session Management is implemented by using a instance of Siebel::SOAP::Auth inside a C<transport_hook> C<sub>, passing to it the original request. The original request will
+be modified, adding necessary authentication data to the SOAP Header. The instance of Siebel::SOAP::Auth will also try to manage the session and token expiration times and
+request a new one before expiration, avoiding a new round-trip to the server for another successful request.
+
+Session Management for calling Siebel web services great improves speed, since regular authentication takes a loot of additional steps. This class will implement the management
+of requesting tokens automatically (but some tuning with the parameters might be necessary).
+
+This class is tight coupled to L<XML::Compile::WSDL11> interface. By using it, it is expected that you will use everything else from L<XML::Compile>.
+
+This class is a L<Moo> class and it uses also L<Log::Report> to provide debug information if required.
+
+=head1 ATTRIBUTES
+
+The following attributes are available:
+
+=head2 token_key
+
+A string of the respective key of the token value after response from the Siebel Server is converted to a hash by L<XML::Compile::WSDL11>.
+
+By default, it is defined as C<'{' . $self->get_header_ns() . '}SessionToken'>.
+
+This is a lazy attribute, as defined by L<Moo>.
+
 =cut
 
 has token_key => ( is => 'lazy', isa => Str, reader => 'get_token_key' );
+
+=head2 header_ns
+
+A string that represents the SOAP Header namespace.
+
+By default it is C<http://siebel.com/webservices>. You might want to change it depending
+on the WSDL (current this is not done automatically).
+
+This is a read/write attribute.
+
+=cut
+
 has header_ns => (
     is      => 'rw',
     isa     => Str,
@@ -27,6 +113,15 @@ has header_ns => (
     reader  => 'get_header_ns',
     writer  => 'set_header_ns'
 );
+
+=head2 user
+
+A string representing the login to be used for web service authentication.
+
+This is a required attribute during object creation.
+
+=cut
+
 has user => (
     is       => 'rw',
     isa      => Str,
@@ -34,6 +129,15 @@ has user => (
     writer   => 'set_user',
     required => 1
 );
+
+=head2 password
+
+A string representing the login password used for authentication.
+
+This is a required attribute during object creation.
+
+=cut
+
 has password => (
     is       => 'rw',
     isa      => Str,
@@ -41,6 +145,15 @@ has password => (
     writer   => 'set_pass',
     required => 1
 );
+
+=head2 token
+
+A string type attribute that holds the token.
+
+All token details are handled internally.
+
+=cut
+
 has token => (
     is      => 'ro',
     isa     => Str,
@@ -48,6 +161,19 @@ has token => (
     writer  => '_set_token',
     default => 'unset'
 );
+
+=head2 lookup_ns
+
+A string type attribute that holds the namespace that must be used to find
+the namespace prefix that should be used to declare the elements for authentication
+in the SOAP header.
+
+By default is set to C<http://schemas.xmlsoap.org/soap/envelope/>.
+
+This is a read/write attribute.
+
+=cut
+
 has lookup_ns => (
     is      => 'rw',
     isa     => Str,
@@ -55,33 +181,110 @@ has lookup_ns => (
     reader  => 'get_lookup_ns',
     writer  => 'set_lookup_ns'
 );
+
+=head2 remain_ttl
+
+A integer that defines the minimum amount of remaining seconds the token should have before asking
+for a new one.
+
+By default it is set to 10 seconds, but you might want to fine tune it depending on several factor, specially
+the average time the request to a web services takes to have an answer.
+
+This is a read-only attribute, so you must set it's value during object creation.
+
+=cut
+
 has remain_ttl =>
   ( is => 'ro', isa => Int, default => 10, reader => 'get_remain_ttl' );
+
+=head2 session_type
+
+A string type attribute that defines the session type to be used.
+
+By default it is set to C<Stateless>.
+
+This is a read-only attribute, so you must set it's value during object creation.
+
+=cut
+
 has session_type => (
     is      => 'ro',
     isa     => Str,
     reader  => 'get_session_type',
     default => 'Stateless'
 );
+
+=head2 last_fault
+
+A string attribute type that holds the error message received from the Siebel Server.
+
+This is a read-only attribute.
+
+=cut
+
 has last_fault => (
     is     => 'ro',
     isa    => Str,
     reader => 'get_last_fault',
     writer => '_set_last_fault'
 );
+
+=head2 auth_fault
+
+A compiled regular expression that is used to match specific SOAP faults returned by the Siebel Server
+that means "token expired".
+
+It is set by default to C</^Error\sCode:\s10944642/>.
+
+=cut
+
 has auth_fault => (
     is      => 'ro',
     isa     => RegexpRef,
     reader  => 'get_auth_fault',
     default => sub { qr/^Error\sCode:\s10944642/ }
 );
+
+=head2 session_timeout
+
+A integer type attribute that defines the Session Timeout to be considered, as defined on the Siebel Server.
+
+By default is set to 900 seconds.
+
+This is a read-only attribute.
+
+=cut
+
 has session_timeout =>
   ( is => 'ro', isa => Int, default => 900, reader => 'get_session_timeout' );
+
+=head2 token_timeout
+
+A integer type attribute that defines the Token Timeout to be considered, as defined on the Siebel Server.
+
+By default is set to 900 seconds.
+
+This is a read-only attribute.
+
+=cut
+
 has token_timeout =>
   ( is => 'ro', isa => Int, default => 900, reader => 'get_token_timeout' );
+
+=head2 token_max_age
+
+A integer type attribute that defines the Token Maximum Age to be considered, as defined on the Siebel Server.
+
+By default is set to 172800 seconds.
+
+This is a read-only attribute.
+
+=cut
+
 has token_max_age =>
   ( is => 'ro', isa => Int, default => 172800, reader => 'get_token_max_age' )
   ;    # 2880 minutes
+
 has _token_birth => (
     is        => 'ro',
     isa       => Num,
@@ -91,12 +294,51 @@ has _token_birth => (
     clearer   => 1
 );
 
+=head1 METHODS
+
+All attributes have their respectiver getters and setters as defined in the Perl Best Practices book.
+
+Of course, read-only attributes have only getters.
+
+This is the list of those methods with a brief explanation:
+
+=over
+
+=item *
+
+get_token_key: getter for C<token_key> attribute.
+
+=item *
+
+get_header_ns: getter for C<header_ns> attribute.
+
+=item *
+
+set_header_ns: setter for C<header_ns> attribute.
+
+=back
+
+There are additional methods as well.
+
+=cut
+
 sub _build_token_key {
 
     my ($self) = @_;
     return '{' . $self->get_header_ns() . '}SessionToken';
 
 }
+
+=head2 add_auth_header
+
+This method B<must> be invoked inside the C<transport_hook> parameter definition during the call to C<compileClient> method
+of L<XML::Compile::WSDL11>.
+
+It expects as parameter the original L<HTTP::Request> object, that will have its payload modified (SOAP Header).
+
+It returns the L<HTTP::Request> with its payload modified.
+
+=cut
 
 sub add_auth_header {
 
@@ -180,6 +422,18 @@ sub add_auth_header {
 
 }
 
+=head2 find_token
+
+Finds and set the token returned by the Siebel Server in the response of a request.
+
+It expects as parameter the hash reference returned by the execution of the code reference created by the
+C<compileClient> method of L<XML::Compile::WSDL11>. This hash reference must have a key as defined by the
+C<token_key> attribute of a instance of Siebel::SOAP::Auth.
+
+Once the token is found, the object updates itself internally. Otherwise an exception will be raised.
+
+=cut
+
 sub find_token {
 
     my ( $self, $answer ) = @_;
@@ -207,12 +461,33 @@ sub find_token {
 
 }
 
+=head2 check_fault
+
+Verifies if an answer from the Siebel Server is a failure of not.
+
+Expects as parameter the answer and raises an exception.
+
+If the fault returned by the server is related to a token expiration, a exception
+will be raised with the text "token expired".
+
+That means you should check for this exception with the C<try> function of L<Log::Report> and
+provide a fallback routine (like resending your request). The instance of Siebel::SOAP::Auth will
+also resets the token status internally to allow reuse.
+
+For other errors, an exception will be created with exactly the same message available in the fault
+element of the SOAP envelope.
+
+=cut
+
 sub check_fault {
 
     my ( $self, $answer ) = @_;
 
     if ( exists( $answer->{Fault} ) ) {
 
+        $self->_set_token('unset');    # sane setting
+        $self->_clear_token_birth();
+        trace 'cleaned up token and token_birth attributes';
         if ( $answer->{Fault}->{faultstring} =~ $self->get_auth_fault() ) {
 
             die 'token expired';
